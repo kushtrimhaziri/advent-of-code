087ad27b5dbbb27feb018703ff9a132e81e78509
Kushtrim Haziri <kushtrim.h@gjirafa.com>
2022-12-11 21:55:45 +0100

    Day 2 Solution

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,12 @@
 ﻿using CSharpStarterPack.Days;
 
-var Day1Part1_Solution = Day01.PuzzleA();
-var Day1Part2_Solution = Day01.PuzzleB();
+//var Day1Part1_Solution = Day01.PuzzleA();
+//var Day1Part2_Solution = Day01.PuzzleB();
+
+//var Day2Part1_Solution = Day02.PuzzleA();
+var Day2Part2_Solution = Day02.PuzzleB();
+
+
+Console.WriteLine(Day2Part2_Solution);
 
 
