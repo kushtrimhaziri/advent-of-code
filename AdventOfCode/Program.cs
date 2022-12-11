157051ae7c58545a4966fc84ab9b3f88442b1be7
Kushtrim Haziri <kushtrim.h@gjirafa.com>
2022-12-11 20:54:03 +0100

    Day 1 solution Upgrade to dotnet 7

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,6 @@
+﻿using CSharpStarterPack.Days;
+
+var Day1Part1_Solution = Day01.PuzzleA();
+var Day1Part2_Solution = Day01.PuzzleB();
+
+
--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -1,4 +1,6 @@
+using CSharpStarterPack.PuzzleInputs;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpStarterPack.Days
@@ -7,11 +9,48 @@ namespace CSharpStarterPack.Days
     {
         public static string PuzzleA(string inputText)
         {
-            return inputText;
+            var lines = PuzzleInput.GetInputAsArray(1);
+            var elvesWithCalories = new Dictionary<int, int>();
+
+            var startElf = 1;
+            var sumOfCalories = 0;
+
+            foreach (var calorie in lines)
+            {
+                if (string.IsNullOrEmpty(calorie))
+                {
+                    elvesWithCalories.Add(startElf, sumOfCalories);
+                    sumOfCalories = 0;
+                    startElf++;
+                    continue;
+                }
+                sumOfCalories += Int32.Parse(calorie);
+            }
+
+            return elvesWithCalories.Max(x => x.Value).ToString();
         }
         public static int PuzzleB()
         {
-            return 2;
+            var lines = PuzzleInput.GetInputAsArray(1);
+            var elvesWithCalories = new Dictionary<int, int>();
+
+            var startElf = 1;
+            var sumOfCalories = 0;
+
+            foreach (var calorie in lines)
+            {
+                if (string.IsNullOrEmpty(calorie))
+                {
+                    elvesWithCalories.Add(startElf, sumOfCalories);
+                    sumOfCalories = 0;
+                    startElf++;
+                    continue;
+                }
+                sumOfCalories += Int32.Parse(calorie);
+            }
+            var threeElvesWithMostCalories = elvesWithCalories.OrderByDescending(x=>x.Value).Take(3).Sum(x=>x.Value);
+
+            return threeElvesWithMostCalories;
         }
     }
 }
