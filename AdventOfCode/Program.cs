0cacbcf34391902bde81e88932553fb2c45b629b
Kushtrim Haziri <kushtrim.h@gjirafa.com>
2023-01-01 19:01:56 +0100

    Day 5 Solution

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -10,9 +10,13 @@
 //var Day3Part2_Solution = Day03.PuzzleB();
 
 
-var Day4Part1_Solution = Day04.PuzzleA();
-var Day4Part2_Solution = Day04.PuzzleB();
+//var Day4Part1_Solution = Day04.PuzzleA();
+//var Day4Part2_Solution = Day04.PuzzleB();
 
-Console.WriteLine(Day4Part1_Solution);
+
+var Day5Part1_Solution = Day05.PuzzleA();
+var Day5Part2_Solution = Day05.PuzzleB();
+
+Console.WriteLine(Day5Part1_Solution);
 
 
