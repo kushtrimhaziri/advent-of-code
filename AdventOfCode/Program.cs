7b80d2576128c281756f92a2dbe4ed0b1484555e
Kushtrim Haziri <kushtrim.h@gjirafa.com>
2023-12-12 02:00:16 +0100

    day 11 solution

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,7 @@
 ﻿using CSharpStarterPack.Days_2023;
 
 
-//var part1 = Day05.PuzzleA();
-var part2 = Day05.PuzzleB();
+var part1 = Day11.PuzzleA();
+var part2 = Day11.PuzzleB();
 
 Console.ReadLine();
--- src/Days/2023/Day11.cs
+++ src/Days/2023/Day11.cs
@@ -1,14 +1,103 @@
+using CSharpStarterPack.PuzzleInputs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace CSharpStarterPack.Days_2023
 {
     public static class Day11
     {
-        public static int PuzzleA()
+        public static string[] Input = PuzzleInput.GetInputAsArray(11, 2023);
+
+        public static double PuzzleA()
         {
-            return 0;
+            var x = Input[0].Length;
+            var y = Input.Length;
+            var galaxies = new List<(int, int)>();
+
+            var noGalaxiesVerticalLines = FillLines(x);
+            var noGalaxiesHorizontalLines = FillLines(y);
+
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    if (Input[i][j] == '#')
+                    {
+                        galaxies.Add((i, j));
+                        noGalaxiesHorizontalLines.Remove(i);
+                        noGalaxiesVerticalLines.Remove(j);
+                    }
+                }
+            }
+
+            var finalGalaxies = GetFinalGalaxiesCoordinates(galaxies, noGalaxiesVerticalLines, noGalaxiesHorizontalLines, 1, 0);
+
+            return GetSumOfLengthsBetweenPoints(finalGalaxies);
         }
-        public static int PuzzleB()
+        public static double PuzzleB()
         {
-            return 0;
+            var x = Input[0].Length;
+            var y = Input.Length;
+            var galaxies = new List<(int, int)>();
+            var noGalaxiesVerticalLines = FillLines(x);
+            var noGalaxiesHorizontalLines = FillLines(y);
+
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    if (Input[i][j] == '#')
+                    {
+                        galaxies.Add((i, j));
+                        noGalaxiesHorizontalLines.Remove(i);
+                        noGalaxiesVerticalLines.Remove(j);
+                    }
+                }
+            }
+
+            var finalGalaxies = GetFinalGalaxiesCoordinates(galaxies, noGalaxiesVerticalLines, noGalaxiesHorizontalLines, 1000000, 1);
+
+            return GetSumOfLengthsBetweenPoints(finalGalaxies);
+        }
+
+        private static List<int> FillLines(int lastLine)
+        {
+            var lines = new List<int>();
+            for (int i = 0; i < lastLine; i++)
+            {
+                lines.Add(i);
+            }
+
+            return lines;
+        }
+
+        private static List<(int, int)> GetFinalGalaxiesCoordinates(List<(int, int)> galaxies, List<int> noGalaxiesVerticalLines, List<int> noGalaxiesHorizontalLines, int incrementor, int nrOfReplacement)
+        {
+            var finalGalaxies = new List<(int, int)>();
+            foreach (var item in galaxies)
+            {
+                var noVerticalLines = noGalaxiesVerticalLines.Where(x => x < item.Item2).Count();
+                var noHorizontalLines = noGalaxiesHorizontalLines.Where(x => x < item.Item1).Count();
+
+                finalGalaxies.Add((item.Item1 + (noHorizontalLines * incrementor) - (noHorizontalLines * nrOfReplacement), item.Item2 + (noVerticalLines * incrementor) - (noVerticalLines * nrOfReplacement)));
+            }
+            return finalGalaxies;
+        }
+
+        private static double GetSumOfLengthsBetweenPoints(List<(int, int)> galaxies)
+        {
+            double sum = 0;
+            for (int i = 0; i < galaxies.Count - 1; i++)
+            {
+                for (int j = i + 1; j < galaxies.Count; j++)
+                {
+                    sum += Math.Abs(galaxies[i].Item1 - galaxies[j].Item1) +
+                           Math.Abs(galaxies[i].Item2 - galaxies[j].Item2);
+                }
+            }
+
+            return sum;
         }
     }
 }
